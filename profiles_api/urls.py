4328342ca442e01fa6eae9747b753c7fb735e948
Arvish <doshi.arvish16@gmail.com>
2020-05-30 12:12:32 +0530

    Added viewset to profiles api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter  # router is a class provided by the Django-restframework in order to generate the different routes that are available for our view set
+
 from profiles_api import views
+
+
+router = DefaultRouter()
+
+router.register('hello_viewset',views.HelloViewSet,base_name="hello-viewset") #last argument(base_name) is used for retrieving the url's
+                                                                               #in our router if we ever need to do that using the url
+                                                                               # retrieving function provided by the Django...
+# we don't use forward slash '/' after hello_viewset because routers will create all 4 url's for us,
 
 urlpatterns = [
 
+
    path('hello_view',views.HelloApiView.as_view()),
+   path('',include(router.urls))
 
 ]
